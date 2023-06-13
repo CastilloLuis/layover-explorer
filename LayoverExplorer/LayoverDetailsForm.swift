114532549f91e0f1a10e58727a03dd184164dbbf
Luis Castillo <luisedcastillog@gmail.com>
2023-06-13 18:10:24 +0200

    Initial commit

---
+++ LayoverExplorer/LayoverDetailsForm.swift
@@ -0,0 +1,22 @@
+//
+//  LayoverDetailsForm.swift
+//  LayoverExplorer
+//
+//  Created by Luis Castillo on 6/13/23.
+//
+
+import SwiftUI
+
+struct LayoverDetailsForm: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct LayoverDetailsForm_Previews: PreviewProvider {
+    static var previews: some View {
+        LayoverDetailsForm()
+    }
+}
---
+++ LayoverExplorer/LayoverExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoverExplorerApp.swift
+//  LayoverExplorer
+//
+//  Created by Luis Castillo on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoverExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
